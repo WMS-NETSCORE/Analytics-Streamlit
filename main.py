9c8fd10ae4366c135aa554862181f74fcc31d7cf
WMS-NETSCORE <85214629+WMS-NETSCORE@users.noreply.github.com>
2021-07-12 15:45:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,25 +21,6 @@ df1['year']=df1['year'].astype(str)
 df1['Color']=df1['Color'].astype(str)
 y=['2017','2018','2019','2020','2021']
 
-def download_link(object_to_download, download_filename, download_link_text):
-    """
-    Generates a link to download the given object_to_download.
-    object_to_download (str, pd.DataFrame):  The object to be downloaded.
-    download_filename (str): filename and extension of file. e.g. mydata.csv, some_txt_output.txt
-    download_link_text (str): Text to display for download link.
-    Examples:
-    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
-    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
-    """
-    if isinstance(object_to_download,pd.DataFrame):
-        object_to_download = object_to_download.to_csv(index=False)
-    # some strings <-> bytes conversions necessary here
-    b64 = base64.b64encode(object_to_download.encode()).decode()
-    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
-
-
-
-
 
 s = st.selectbox("Reports As of Today",("Select Options","By Item", "Top 10 Shipping Cities"))
 if st.checkbox("Report As of Today"):
@@ -71,9 +52,7 @@ if st.checkbox("Show/Hide"):
                           rg['total'] = rg.sum(axis=1)
                           st.write("Displaying Data of ",i,"Pattren with Color")
                           st.table(rg)
-                          if st.button('Download Dataframe as CSV'):
-                              tmp_download_link = download_link(rg, 'Rugs_Report.csv', 'Click here to download your data!')
-                              st.markdown(tmp_download_link, unsafe_allow_html=True)
+                         
                                 
                       elif 'Shipping City' in metric:
                           sc=df1.loc[(df1['Class (no hierarchy)'] == i) & (df1['year'] == j),['Shipping City']].value_counts().head(10)
